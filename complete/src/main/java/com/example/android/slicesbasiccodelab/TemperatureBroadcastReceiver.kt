13d32cc6447511a768918ccc385b7f486ad7aa6d
markgray <mark.t.gray@gmail.com>
2021-06-20 07:42:39 -0400

    Added comments to TemperatureBroadcastReceiver.kt in "complete" module

--- complete/src/main/java/com/example/android/slicesbasiccodelab/TemperatureBroadcastReceiver.kt
+++ complete/src/main/java/com/example/android/slicesbasiccodelab/TemperatureBroadcastReceiver.kt
@@ -23,23 +23,49 @@ import com.example.android.slicesbasiccodelab.MainActivity.Companion.getTemperat
 import com.example.android.slicesbasiccodelab.MainActivity.Companion.updateTemperature
 
 /**
- * Updates temperature (triggered by SliceProvider and MainActivity).
+ * Updates temperature (triggered by [TemperatureSliceProvider] and [MainActivity]). It is named as
+ * a [BroadcastReceiver] by a `receiver` element in our AndroidManifest.xml file.
  */
 class TemperatureBroadcastReceiver : BroadcastReceiver() {
 
+    /**
+     * This method is called when the [BroadcastReceiver] is receiving an [Intent] broadcast. This
+     * method is always called within the main thread of its process, unless you explicitly asked
+     * for it to be scheduled on a different thread using [Context.registerReceiver]. If the action
+     * of our [Intent] parameter [intent] is [ACTION_CHANGE_TEMPERATURE] we initialize our [Int]
+     * variable `val newValue` to the value stored under the key [EXTRA_TEMPERATURE_VALUE] in the
+     * extras of [intent] using the current value of temperature returned by the [getTemperature]
+     * method as the default. Then we call the [updateTemperature] method with our [Context] paramer
+     * [context] and `newValue` to have it update the temperature.
+     *
+     * @param context The [Context] in which the receiver is running.
+     * @param intent The [Intent] being received.
+     */
     override fun onReceive(context: Context, intent: Intent) {
 
         if (ACTION_CHANGE_TEMPERATURE == intent.action) {
-            val newValue =
+            val newValue: Int =
                 intent.extras?.getInt(EXTRA_TEMPERATURE_VALUE, getTemperature()) ?: return
             updateTemperature(context, newValue)
         }
     }
 
     companion object {
+        /**
+         * Our package name, which we use as the namespace for [ACTION_CHANGE_TEMPERATURE] and
+         * [EXTRA_TEMPERATURE_VALUE].
+         */
         private const val PACKAGE_NAME = "com.example.android.slicesbasiccodelab"
 
+        /**
+         * The [Intent] action that our [onReceive] override understands.
+         */
         const val ACTION_CHANGE_TEMPERATURE = "$PACKAGE_NAME.action.CHANGE_TEMPERATURE"
+
+        /**
+         * The key under which the new temperature is stored as an extra in the broadcast [Intent]
+         * which our [onReceive] override receives.
+         */
         const val EXTRA_TEMPERATURE_VALUE = "$PACKAGE_NAME.extra.TEMPERATURE_VALUE"
     }
 }
