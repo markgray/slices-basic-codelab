6d4336482a347b6cb4483189c659678f03855682
markgray <mark.t.gray@gmail.com>
2025-04-19 06:34:21 -0400

    Updated gradle dependencies

--- base/build.gradle.kts
+++ base/build.gradle.kts
@@ -51,7 +51,7 @@ android {
 dependencies {
     implementation("androidx.activity:activity-ktx:1.10.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.core:core-ktx:1.16.0")
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
 
     // The slice builder ktx library has a number of dependencies. For reference, since this is a
